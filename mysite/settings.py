3d8113aecdfe2ab33d9931cd68a80dbb1d2e8a29
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-09-04 20:41:32 +0530

    added all

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-woh$u)53@_!3_c06ix-*&+0^*g*veq%o#nbkjb53924f*=&@vh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ ]
 
